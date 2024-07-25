f42a985872607f597e092daffc9d15a7389c1ef8
Aly Badawy <AlyBadawy@users.noreply.github.com>
2024-07-25 06:42:25 -0400

    [HORUS] Add ADC drivers (With Calibrations)

--- horus/Core/Src/gpio.c
+++ horus/Core/Src/gpio.c
@@ -48,11 +48,21 @@ void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, Av3Ld_Pin|Av2Ld_Pin|Av1Ld_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, ActDa_Pin|ActLa_Pin|ActCk_Pin|ActLd_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PAPin PAPin PAPin */
+  GPIO_InitStruct.Pin = Av3Ld_Pin|Av2Ld_Pin|Av1Ld_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = Boot1_Pin;
--- horus/Core/Src/main.c
+++ horus/Core/Src/main.c
@@ -19,6 +19,8 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "adc.h"
+#include "dma.h"
 #include "rtc.h"
 #include "gpio.h"
 
@@ -36,7 +38,16 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define VREFINT      		1.22
+#define ADC_RESOLUTION    	4096	// 12-bit ADC resolution
 
+
+#define V25					0.76
+#define CPU_TEMP_AVG_SLOPE  0.0025
+
+#define adcR1               33000.0
+#define adcR2               3300.0
+#define adcRatio			(adcR2 / (adcR1 + adcR2))
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -47,6 +58,12 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+uint32_t adcValues[5];	// buffer to store the AVs values from ADC, an array of 5 values
+//  adcValues[0]  		<< value for VREFINT to calibrate VREF
+//  adcValues[1]  		<< value for Internal temperature (CPU)
+//  adcValues[2]  		<< value for AV1
+//  adcValues[3]  		<< value for Av2
+//  adcValues[4]  		<< value for Av3
 
 /* USER CODE END PV */
 
@@ -66,6 +83,35 @@ int _write(int file, char *ptr, int len) {
 	}
 	return len;
 }
+
+float getVref(uint32_t adcValue) {
+	float value = adcValue;
+	return (VREFINT * ADC_RESOLUTION) / value;
+}
+
+float getCpuTempC(uint32_t vrefValue, uint32_t adcValue) {
+	float vref = getVref(vrefValue);
+	float temp_sense = (vref/ADC_RESOLUTION) * adcValue;
+	return (((V25 - temp_sense) / CPU_TEMP_AVG_SLOPE) + 25.0);
+}
+
+float cToF(float c) {
+	return (c * (9.0 / 5.0)) + 32.0;
+}
+
+float fToC(float f) {
+	return (f - 32.0) * (5.0 / 9.0);
+}
+
+float adcToAv(uint32_t vrefValue, uint32_t adcValue) {
+	float vref = getVref(vrefValue);
+	float voltage = ((adcValue * vref) / ADC_RESOLUTION) / adcRatio;
+	if (voltage < 0.05) { voltage = 0.0; }
+	return voltage;
+}
+
+
+
 /* USER CODE END 0 */
 
 /**
@@ -76,7 +122,6 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
-
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -97,7 +142,9 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_RTC_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
 
   // Actuators test
@@ -114,6 +161,29 @@ int main(void)
 
   // printf test
   printf("Hello from VexUF\r\n");
+
+  // ADCs test
+  HAL_ADC_Start_DMA(&hadc1, adcValues, 5);
+
+  uint32_t vrefValue = adcValues[0];
+  float vrefValueFloat = adcValues[0];
+  printf("value of VREF: %f which is %0.3fV\r\n", vrefValueFloat, getVref(vrefValue));
+
+  float tempratureValue = adcValues[1];
+  float cpuTempC = getCpuTempC(vrefValue, tempratureValue);
+  float cpuTempF = cToF(cpuTempC);
+  printf("value of temperature: %f, which is %0.3fC, and %0.3fF\r\n", tempratureValue, cpuTempC, cpuTempF);
+
+  float av1 = adcValues[2];
+  float av2 = adcValues[3];
+  float av3 = adcValues[4];
+  printf("value of av1: %f, which is %0.3fV\r\n", av1, adcToAv(vrefValue, av1));
+  printf("value of av2: %f, which is %0.3fV\r\n" ,av2, adcToAv(vrefValue, av2));
+  printf("value of av3: %f, which is %0.3fV\r\n", av3, adcToAv(vrefValue, av3));
+
+  HAL_ADC_Stop_DMA(&hadc1);
+
+  HAL_Delay(1000);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -147,7 +217,12 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.LSIState = RCC_LSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 84;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -157,12 +232,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
