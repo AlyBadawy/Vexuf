c17d8b7f2f1139837dab0a7e37d1855258afa3c7
Aly Badawy <AlyBadawy@users.noreply.github.com>
2024-07-26 12:57:29 -0400

    [HORUS] I2C peripheral and I2C Checker (#3)

---
+++ horus/Core/Inc/i2c_checker.h
@@ -0,0 +1,16 @@
+/*
+ * i2c_checker.h
+ *
+ *  Created on: Jul 26, 2024
+ *      Author: alybadawy
+ */
+
+#ifndef INC_I2C_CHECKER_H_
+#define INC_I2C_CHECKER_H_
+
+#include "main.h"
+
+
+void I2C_Scan(I2C_HandleTypeDef *hi2c);
+
+#endif /* INC_I2C_CHECKER_H_ */
---
+++ horus/Core/Src/i2c_checker.c
@@ -0,0 +1,23 @@
+/*
+ * i2c_checker.c
+ *
+ *  Created on: Jul 26, 2024
+ *      Author: alybadawy
+ */
+
+#include "i2c_checker.h"
+
+// Function to scan I2C bus for devices
+void I2C_Scan(I2C_HandleTypeDef *hi2c) {
+    uint8_t i2c_address;
+    HAL_StatusTypeDef result;
+    printf("Scanning I2C bus...\n");
+    for (i2c_address = 1; i2c_address < 128; i2c_address++) {
+        result = HAL_I2C_IsDeviceReady(hi2c, (i2c_address << 1), 1, 10);
+        if (result == HAL_OK) {
+            printf("I2C found 0x%02X\n", i2c_address);
+        }
+    }
+    printf("I2C scan completed.\n");
+}
+
